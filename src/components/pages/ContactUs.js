41edd90738a55da39aa21e74f96330155f34f4d7
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-03-21 16:01:56 +0700

    Blog, Contact, Instructor

---
+++ src/components/pages/ContactUs.js
@@ -0,0 +1,236 @@
+import Layout from "../layouts";
+
+function ContactUs() {
+    return (
+        <Layout title="Contact Us">
+            <div className="rbt-banner-area rbt-banner-8 variation-02">
+                <div className="container">
+                    <div className="row g-5 align-items-center">
+                        <div className="col-lg-10 offset-lg-1">
+                            <div className="content">
+                                <div className="inner text-center">
+                                    <div className="rbt-new-badge rbt-new-badge-one">
+                                        <span className="rbt-new-badge-icon">🏆</span> The Leader in Online Learning
+                                    </div>
+
+                                    <h1 className="title">
+                                        Read About Our
+                                        <span className="header-caption">
+                                            <span className="cd-headline clip is-full-width">
+                                                <span className="cd-words-wrapper" style={{ width: "241px" }}>
+                                                    <b className="theme-gradient is-visible">Mission.</b>
+                                                    <b className="theme-gradient is-hidden">Vission.</b>
+                                                    <b className="theme-gradient is-hidden">Planning.</b>
+                                                </span>
+                                            </span>
+                                        </span>
+                                    </h1>
+                                    <p className="description has-medium-font-size mt--20">
+                                        Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!
+                                    </p>
+                                    <div className="slider-btn rbt-button-group justify-content-center">
+                                        <a className="rbt-btn btn-gradient hover-icon-reverse" href="#!">
+                                            <span className="icon-reverse-wrapper">
+                                                <span className="btn-text">Log in to Start</span>
+                                                <span className="btn-icon">
+                                                    <i className="feather-arrow-right"></i>
+                                                </span>
+                                                <span className="btn-icon">
+                                                    <i className="feather-arrow-right"></i>
+                                                </span>
+                                            </span>
+                                        </a>
+                                        <a className="rbt-btn hover-icon-reverse btn-white" href="#!">
+                                            <span className="icon-reverse-wrapper">
+                                                <span className="btn-text">Contact US</span>
+                                                <span className="btn-icon">
+                                                    <i className="feather-arrow-right"></i>
+                                                </span>
+                                                <span className="btn-icon">
+                                                    <i className="feather-arrow-right"></i>
+                                                </span>
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rbt-video-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="video-popup-wrapper">
+                                <img className="w-100 rbt-radius" src="assets/images/bg/bg-image-12.jpg" alt="Video" />
+                                <a className="rbt-btn rounded-player-2 popup-video position-to-top with-animation btn-theme-color" href="https://www.youtube.com/watch?v=nA1Aqp0sPQo">
+                                    <span className="play-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rbt-about-area about-style-1 bg-color-white rbt-section-gap">
+                <div className="container">
+                    <div className="row g-5 align-items-center">
+                        <div className="col-lg-6">
+                            <div className="content">
+                                <img src="assets/images/about/about-06.png" alt="About" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 sal-animate" data-sal="slide-up" data-sal-duration="700">
+                            <div className="inner pl--50 pl_sm--5">
+                                <div className="content text-start">
+                                    <span className="rbt-badge-6 bg-primary-opacity">About Histudy</span>
+                                    <h3 className="title">Welcome to Histudy for your skills with best Online courses.</h3>
+                                    <p className="description mt--30">
+                                        <strong>Histudy educational platform</strong> ipsum dolor sit amet consectetur adipisicing elit.
+                                    </p>
+                                    <p className="description">
+                                        Nam inventore praesentium alias incidunt! Veritatis, necessitatibus fuga dolore tenetur, beatae suscipit fugit est ea perspiciatis quo provident nisi dolor
+                                        similique architecto nihil.
+                                    </p>
+                                    <div className="read-more-btn mt--40">
+                                        <a className="rbt-btn rbt-marquee-btn radius-round btn-gradient" href="#!">
+                                            <span data-text="Start Learning For Free">Start Learning For Free</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rbt-call-to-action-area rbt-section-gap bg-gradient-8">
+                <div className="rbt-callto-action rbt-cta-default style-6">
+                    <div className="container">
+                        <div className="row g-5 align-items-center content-wrapper">
+                            <div className="col-xxl-3 col-xl-3 col-lg-6">
+                                <div className="inner">
+                                    <div className="content text-start">
+                                        <h2 className="title color-white mb--0">Scholarship Programs</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xxl-6 col-xl-6 col-lg-6">
+                                <div className="inner-content text-start">
+                                    <p className="color-white">
+                                        Apply for Admission in Post Graduate Diploma. Application Deadline: 26th September 2023 · Undergraduate &amp; Graduate Admission: Fall 2023 Semester · 20%
+                                        Waiver on ...
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-xxl-3 col-xl-3 col-lg-6">
+                                <div className="call-to-btn text-start text-xl-end">
+                                    <a className="rbt-btn btn-white hover-icon-reverse" href="#!">
+                                        <span className="icon-reverse-wrapper">
+                                            <span className="btn-text">Histudy Financial Aid</span>
+                                            <span className="btn-icon">
+                                                <i className="feather-arrow-right"></i>
+                                            </span>
+                                            <span className="btn-icon">
+                                                <i className="feather-arrow-right"></i>
+                                            </span>
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rbt-service-area bg-color-white rbt-section-gap">
+                <div className="container">
+                    <div className="row mb--60">
+                        <div className="col-lg-12">
+                            <div className="section-title text-center">
+                                <h2 className="title">Why learn with our courses?</h2>
+                                <p className="description mt--30">
+                                    <strong>Histudy educational platform</strong> ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row row--15 mt_dec--30">
+                        <div className="col-lg-4 col-xl-3 col-xxl-3 col-md-6 col-sm-6 col-12 mt--30">
+                            <div className="service-card service-card-6">
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img src="assets/images/icons/001-bulb.png" alt="icons" />
+                                    </div>
+                                    <div className="content">
+                                        <h6 className="title">
+                                            <a href="#!">Your Apply</a>
+                                        </h6>
+                                        <p className="description">English Learning looking for random paragraphs, you've come to the right place.</p>
+                                    </div>
+                                    <span className="number-text">1</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 col-xl-3 col-xxl-3 col-md-6 col-sm-6 col-12 mt--30">
+                            <div className="service-card service-card-6">
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img src="assets/images/icons/002-hat.png" alt="Shape" />
+                                    </div>
+                                    <div className="content">
+                                        <h6 className="title">
+                                            <a href="#!">We Connect</a>
+                                        </h6>
+                                        <p className="description">Javascript Learning looking for random paragraphs, you've come to the right place.</p>
+                                    </div>
+                                    <span className="number-text">2</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 col-xl-3 col-xxl-3 col-md-6 col-sm-6 col-12 mt--30">
+                            <div className="service-card service-card-6">
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img src="assets/images/icons/003-id-card.png" alt="Shape" />
+                                    </div>
+                                    <div className="content">
+                                        <h6 className="title">
+                                            <a href="#!">You Get Ready</a>
+                                        </h6>
+                                        <p className="description">Angular Learning looking for random paragraphs, you've come to the right place.</p>
+                                    </div>
+                                    <span className="number-text">3</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 col-xl-3 col-xxl-3 col-md-6 col-sm-6 col-12 mt--30">
+                            <div className="service-card service-card-6">
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img src="assets/images/icons/004-pass.png" alt="Shape" />
+                                    </div>
+                                    <div className="content">
+                                        <h6 className="title">
+                                            <a href="#!">Completed</a>
+                                        </h6>
+                                        <p className="description">Php Learning looking for random paragraphs, you've come to the right place.</p>
+                                    </div>
+                                    <span className="number-text">4</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default ContactUs;
---
+++ src/components/pages/Instructor/index.js
@@ -0,0 +1,210 @@
+import { Link } from "react-router-dom";
+import Layout from "../../layouts/index";
+function Instructor() {
+    return (
+        <Layout title="Instructor">
+            <div className="rbt-breadcrumb-default ptb--100 ptb_md--50 ptb_sm--30 bg-gradient-1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="breadcrumb-inner text-center">
+                                <h2 className="title">Instructor</h2>
+                                <ul className="page-list">
+                                    <li className="rbt-breadcrumb-item">
+                                        <Link to="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <div className="icon-right">
+                                            <i className="feather-chevron-right"></i>
+                                        </div>
+                                    </li>
+                                    <li className="rbt-breadcrumb-item active">Instructor</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rbt-team-area bg-color-extra2 rbt-section-gap">
+                <div className="container">
+                    <div className="row g-5">
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-01.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h4 className="title">Zohaib Oneill</h4>
+                                        <h6 className="subtitle theme-gradient">Math Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-02.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Alvin Rivera</h2>
+                                        <h6 className="subtitle theme-gradient">Depertment Head</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-03.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Joao Lloyd</h2>
+                                        <h6 className="subtitle theme-gradient">Math Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-04.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Bella</h2>
+                                        <h6 className="subtitle theme-gradient">Math Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-05.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Claudia Pruitt</h2>
+                                        <h6 className="subtitle theme-gradient">Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-06.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Judy Oneill</h2>
+                                        <h6 className="subtitle theme-gradient">Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-07.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Levi Arm</h2>
+                                        <h6 className="subtitle theme-gradient">Teacher</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="rbt-team team-style-default style-three small-layout rbt-hover">
+                                <div className="inner">
+                                    <div className="thumbnail">
+                                        <img src="assets/images/team/team-08.jpg" alt="Corporate Template" />
+                                    </div>
+                                    <div className="content">
+                                        <h2 className="title">Fred Guer</h2>
+                                        <h6 className="subtitle theme-gradient">Economic</h6>
+                                        <span className="team-form">
+                                            <i className="feather-map-pin"></i>
+                                            <span className="location">CO Miego, AD,USA</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-12 mt--60">
+                            <nav>
+                                <ul className="rbt-pagination">
+                                    <li>
+                                        <a href="#!" aria-label="Previous">
+                                            <i className="feather-chevron-left"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">1</a>
+                                    </li>
+                                    <li className="active">
+                                        <a href="#!">2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" aria-label="Next">
+                                            <i className="feather-chevron-right"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default Instructor;
