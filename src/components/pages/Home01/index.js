98bcf9813a07352f9dfad1e097003e2f214f15e0
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-03-21 23:27:15 +0700

    Fix import 02

--- src/components/pages/Home01/index.js
+++ src/components/pages/Home01/index.js
@@ -1,15 +1,15 @@
 import Layout from "../../layouts";
-import AboutUs from "../../views/Home/AboutUs";
-import Banner from "../../views/Home/Banner";
-import Blog from "../../views/Home/Blog";
-import CallToAction from "../../views/Home/CallToAction";
-import Categories from "../../views/Home/Categories";
-import Course from "../../views/Home/Course";
-import Education from "../../views/Home/Education";
-import Event from "../../views/Home/Event";
-import Newsletter from "../../views/Home/Newsletter";
-import Teacher from "../../views/Home/Teacher";
-import WhyChoose from "../../views/Home/WhyChoose";
+import AboutUs from "../../views/Home01/AboutUs";
+import Banner from "../../views/Home01/Banner";
+import Blog from "../../views/Home01/Blog";
+import CallToAction from "../../views/Home01/CallToAction";
+import Categories from "../../views/Home01/Categories";
+import Course from "../../views/Home01/Course";
+import Education from "../../views/Home01/Education";
+import Event from "../../views/Home01/Event";
+import Newsletter from "../../views/Home01/Newsletter";
+import Teacher from "../../views/Home01/Teacher";
+import WhyChoose from "../../views/Home01/WhyChoose";
 
 function Home() {
     return (
