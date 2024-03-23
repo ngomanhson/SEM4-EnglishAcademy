69d73e9a00f3c9d9712dbaed9e2c55eb6c41ecb3
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-03-23 22:23:23 +0700

    Update custom control video

--- src/components/pages/Lesson/VideoLesson.js
+++ src/components/pages/Lesson/VideoLesson.js
@@ -9,8 +9,14 @@ function VideoLesson(props) {
     const [duration, setDuration] = useState(0);
     const [volume, setVolume] = useState(1);
     const [muted, setMuted] = useState(false);
-    const handlePlayPause = () => {
-        setPlaying(!playing);
+    const [controlsVisible, setControlsVisible] = useState(true);
+
+    const handlePlay = () => {
+        setPlaying(true);
+    };
+
+    const handlePause = () => {
+        setPlaying(false);
     };
 
     const handleProgressChange = (e) => {
@@ -56,61 +62,68 @@ function VideoLesson(props) {
     };
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentTime(playerRef.current.getCurrentTime());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
+        const hideControlsTimeout = setTimeout(() => {
+            setControlsVisible(false);
+        }, 3000);
+
+        return () => clearTimeout(hideControlsTimeout);
+    }, [controlsVisible]);
+
+    const showControls = () => {
+        setControlsVisible(true);
+    };
 
     return (
-        <div className="plyr plyr--full-ui plyr--video plyr--youtube plyr--fullscreen-enabled plyr--paused plyr--stopped plyr__poster-enabled">
-            <div className="plyr__controls">
-                <button type="button" className="plyr__controls__item plyr__control" onClick={handlePlayPause}>
-                    {playing ? <i className="feather-pause"></i> : <i className="feather-play"></i>}
-                </button>
+        <div className="plyr plyr--full-ui plyr--video plyr--youtube plyr--fullscreen-enabled plyr--paused plyr--stopped plyr__poster-enabled" onMouseMove={showControls} onTouchStart={showControls}>
+            {controlsVisible && (
+                <div className="plyr__controls">
+                    <button type="button" className="plyr__controls__item plyr__control" onClick={playing ? handlePause : handlePlay}>
+                        {playing ? <i className="feather-pause"></i> : <i className="feather-play"></i>}
+                    </button>
 
-                <div className="plyr__controls__item plyr__progress__container">
-                    <div className="plyr__progress">
+                    <div className="plyr__controls__item plyr__progress__container">
+                        <div className="plyr__progress">
+                            <input
+                                type="range"
+                                min={0}
+                                max={1}
+                                step="any"
+                                value={progress}
+                                onChange={handleProgressChange}
+                                style={{
+                                    "--value": `${progress * 100}%`,
+                                }}
+                            />
+                        </div>
+                    </div>
+
+                    <span className="plyr__controls__item plyr__time--current">
+                        {formatTime(currentTime)} / {formatTime(duration)}
+                    </span>
+
+                    <div className="plyr__controls__item plyr__volume">
+                        <button type="button" className="plyr__control" onClick={handleMuteToggle}>
+                            {muted ? <i className="feather-volume-x"></i> : <i className="feather-volume-2"></i>}
+                        </button>
                         <input
                             type="range"
                             min={0}
                             max={1}
                             step="any"
-                            value={progress}
-                            onChange={handleProgressChange}
+                            value={volume}
+                            onChange={handleVolumeChange}
                             style={{
-                                "--value": `${progress * 100}%`,
+                                "--value": `${volume * 100}%`,
                             }}
                         />
                     </div>
-                </div>
 
-                <span className="plyr__controls__item plyr__time--current">
-                    {formatTime(currentTime)} / {formatTime(duration)}
-                </span>
-
-                <div className="plyr__controls__item plyr__volume">
-                    <button type="button" className="plyr__control" onClick={handleMuteToggle}>
-                        {muted ? <i className="feather-volume-x"></i> : <i className="feather-volume-2"></i>}
+                    <button className="plyr__controls__item plyr__control" type="button">
+                        <i className="feather-maximize"></i>
+                        {/* <i className="feather-minimize"></i> */}
                     </button>
-                    <input
-                        type="range"
-                        min={0}
-                        max={1}
-                        step="any"
-                        value={volume}
-                        onChange={handleVolumeChange}
-                        style={{
-                            "--value": `${volume * 100}%`,
-                        }}
-                    />
                 </div>
-
-                <button className="plyr__controls__item plyr__control" type="button">
-                    <i className="feather-maximize"></i>
-                    {/* <i className="feather-minimize"></i> */}
-                </button>
-            </div>
+            )}
 
             <div className="plyr__video-wrapper plyr__video-embed" style={{ aspectRatio: 16 / 9 }}>
                 <ReactPlayer
@@ -122,6 +135,8 @@ function VideoLesson(props) {
                     muted={muted}
                     onDuration={handleDuration}
                     onProgress={handleProgress}
+                    onPlay={handlePlay}
+                    onPause={handlePause}
                     width="100%"
                     height="auto"
                     config={{
